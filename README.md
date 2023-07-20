50f841dc648d6475bb58fb3888360607caa543f3
Matt Lawler <matthewlawler99@gmail.com>
2023-07-20 16:16:56 -0700

    Restarting this repo

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# react-async-demo
-A demo of how to use AssemblyAI's async API in JavaScript and React.
+# AssemblyAI Async Transcription in React
+
+## Setup
+To run this demo app, you'll first need a few tools to get your dev environment set up. The first, and most important, is [Homebrew](https://brew.sh/), the third-party package manager for macOS. You can find the installation command there, which you can run in a Terminal window.
+
+Then you can install [Node](https://formulae.brew.sh/formula/node). Running the command found in the link will automatically install the latest Node.js version on your system, which this project is compatible with.
+
+After Homebrew and Node are installed and ready, you'll need to install [Yarn](https://formulae.brew.sh/formula/yarn). It works essentially identically to [npm](https://docs.npmjs.com/downloading-and-installing-node-js-and-npm), but is a bit more optimized for React-based projects, and can have a nicer user-experience compared to npm.
+
+Once you've got those three prerequisites, you're ready to build the project and run it locally!
+
+## Running the Demo App
+To run this demo app, navigate to where you cloned this repo to and at the top level of the directory, run `yarn install`. This command will fetch all of the necessary dependencies and store them in your `node_modules` folder in the same directory. Once this has finished, you can run `yarn start` and the demo app will start up on your local machine, and will automatically open a browser window for you to find it.
+
+## Linking your AssemblyAI API Key
+You'll likely notice upon first trying to use the demo app that you're not authenticated. In case you haven't already, you can sign up for an AssemblyAI account [here](https://www.assemblyai.com/dashboard/signup) and find your API key on your [account dashboard](https://www.assemblyai.com/app/account).
+
+Copy that API key and replace the two "YOUR_API_KEY_HERE" placeholders in `App.js`. This will make sure that you're able to use the API and monitor the status of all of your requests in your [processing queue](https://www.assemblyai.com/app/processing-queue) on your dashboard. Now you're ready to experiment with the async API!
+
+## Further Customization
+Considering this demo is open-source and you can modify the API requests, feel free to explore our [documentation](https://www.assemblyai.com/docs) and learn more about the different parameters that you can send to the async API. You can also customize the kind of output you're expecting depending on your needs.
+
+Thank you for checking out this demo, and if you have any questions, don't hesitate to reach out to us at support@assemblyai.com. Best of luck!
---
+++ src/App.js
@@ -0,0 +1,209 @@
+import "./App.css";
+import axios from "axios";
+import Logo from "./assembly_logo.png";
+import React from "react";
+import { LoadingButton } from "@mui/lab";
+import { CircularProgress, Input, TextField } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+async function transcript(audio_url) {
+  let error_thrown = false;
+  const headers = {
+    authorization: "YOUR_API_KEY_HERE",
+  };
+  const response = await axios
+    .post(
+      "https://api.assemblyai.com/v2/transcript",
+      { audio_url: audio_url },
+      { headers: headers }
+    )
+    .catch((error) => {
+      alert(error.response.data.error);
+      error_thrown = true;
+    });
+  if (error_thrown) {
+    return null;
+  }
+  const transcriptID = response.data.id;
+  const pollingEndpoint = `https://api.assemblyai.com/v2/transcript/${transcriptID}`;
+  while (true) {
+    const pollingResponse = await axios.get(pollingEndpoint, { headers });
+    const transcriptionResult = pollingResponse.data;
+    if (transcriptionResult.status === "completed") {
+      return transcriptionResult;
+    } else if (transcriptionResult.status === "error") {
+      alert(transcriptionResult.error);
+      return null;
+    } else {
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+    }
+  }
+}
+
+async function upload(file) {
+  let error_thrown = false;
+  const headers = {
+    authorization: "YOUR_API_KEY_HERE",
+  };
+  const uploadResponse = await axios
+    .post("https://api.assemblyai.com/v2/upload", file, { headers: headers })
+    .catch((error) => {
+      alert(error.response.data.error);
+      error_thrown = true;
+    });
+  if (error_thrown) {
+    return null;
+  }
+  const upload_url = uploadResponse.data.upload_url;
+  return transcript(upload_url);
+}
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ffffff",
+    },
+  },
+});
+
+export default function App() {
+  const [audioUrl, setAudioUrl] = React.useState("");
+  const [isUploadError, setIsUploadError] = React.useState(false);
+  const [isUploadLoading, setIsUploadLoading] = React.useState(false);
+  const [isUrlError, setIsUrlError] = React.useState(false);
+  const [isUrlLoading, setIsUrlLoading] = React.useState(false);
+  const [uploadTranscriptText, setUploadTranscriptText] = React.useState("");
+  const [uploadedFile, setUploadedFile] = React.useState("");
+  const [urlTranscriptText, setUrlTranscriptText] = React.useState("");
+
+  const onClickSubmit = (type) => () => {
+    if (type === "audio_url" && audioUrl === "") {
+      setIsUrlError(true);
+    } else if (type === "audio_url" && audioUrl !== "") {
+      setIsUrlError(false);
+      setIsUrlLoading(true);
+      transcript(audioUrl).then((result) => {
+        setIsUrlLoading(false);
+        if (result !== null) {
+          setUrlTranscriptText(result.text);
+        }
+      });
+    } else if (type === "uploaded_file" && uploadedFile === "") {
+      setIsUploadError(true);
+    } else if (type === "uploaded_file" && uploadedFile !== "") {
+      setIsUploadError(false);
+      setIsUploadLoading(true);
+      upload(uploadedFile).then((result) => {
+        setIsUploadLoading(false);
+        if (result !== null) {
+          setUploadTranscriptText(result.text);
+        }
+      });
+    }
+  };
+
+  const onChangeUrl = (event) => {
+    setAudioUrl(event.target.value);
+  };
+
+  const readFile = (event) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      setUploadedFile(reader.result);
+    };
+    try {
+      reader.readAsArrayBuffer(event.target.files[0]);
+    } catch {
+      console.error("FileReader attempted to read an invalid file.");
+    }
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div className={"app"}>
+        <div className={"content"}>
+          <img alt={"AssemblyAI Logo"} className={"logo"} src={Logo} />
+          <p>Welcome to AssemblyAI's React demo!</p>
+          <p>
+            This will show you how to use our async API within a React
+            project...
+          </p>
+          <div className={"split"}>
+            <div className={"pane"}>
+              <p>
+                ...via our{" "}
+                <a
+                  className={"link"}
+                  href={
+                    "https://www.assemblyai.com/docs/Models/speech_recognition#quickstart"
+                  }
+                >
+                  audio_url parameter
+                </a>
+                ...
+              </p>
+              <TextField
+                error={isUrlError}
+                focused={true}
+                label={"URL"}
+                onChange={onChangeUrl}
+                sx={{ input: { color: "#ffffff" }, width: "50%" }}
+                value={audioUrl}
+              />
+              <div className={"button-holder"}>
+                <LoadingButton
+                  loading={isUrlLoading}
+                  loadingIndicator={<CircularProgress color={"primary"} />}
+                  onClick={onClickSubmit("audio_url")}
+                  variant={"outlined"}
+                >
+                  Submit
+                </LoadingButton>
+              </div>
+              {urlTranscriptText !== "" ? (
+                <div className={"transcript-holder"}>
+                  <p className={"transcript"}>{urlTranscriptText}</p>
+                </div>
+              ) : null}
+            </div>
+            <div className={"pane"}>
+              <p>
+                ...or by{" "}
+                <a
+                  className={"link"}
+                  href={
+                    "https://www.assemblyai.com/docs/Guides/transcribing_an_audio_file"
+                  }
+                >
+                  uploading a file!
+                </a>
+              </p>
+              <Input
+                defaultValue={uploadedFile}
+                error={isUploadError}
+                onChange={readFile}
+                sx={{ input: { color: "#ffffff" }, width: "50%" }}
+                type={"file"}
+              />
+              <div className={"button-holder"}>
+                <LoadingButton
+                  loading={isUploadLoading}
+                  loadingIndicator={<CircularProgress color={"primary"} />}
+                  onClick={onClickSubmit("uploaded_file")}
+                  variant={"outlined"}
+                >
+                  Submit
+                </LoadingButton>
+              </div>
+              {uploadTranscriptText !== "" ? (
+                <div className={"transcript-holder"}>
+                  <p className={"transcript"}>{uploadTranscriptText}</p>
+                </div>
+              ) : null}
+            </div>
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}

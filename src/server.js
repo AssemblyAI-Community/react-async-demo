62a1e1c315985cffbf6bbca0ac9dfeb19aa30909
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2023-11-29 14:03:28 -0500

    Use AssemblyAI SDK

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
-const axios = require("axios");
+require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
-const fs = require("fs");
+const fs = require("fs/promises");
 const multer = require("multer");
+const { AssemblyAI } = require("assemblyai");
+
+const client = new AssemblyAI({ apiKey: process.env.ASSEMBLYAI_API_KEY });
 
 const app = express();
 app.use(express.json());
@@ -10,54 +13,30 @@ app.use(cors());
 
 // Set file upload destination for temp storage.
 // Must use dest, otherwise files will be stored in memory.
-const upload = multer({ dest: "." });
-
-const headers = { authorization: "YOUR_API_KEY_HERE" };
-
-// Class to standardize error handling between AAI errors and Axios errors.
-class AssemblyAIError {
-  constructor(status, error) {
-    this.response = { status, data: { error: error } };
-  }
-}
+const upload = multer({ dest: "temp" });
 
 app.post("/transcript", async (req, res) => {
   try {
-    const postResponse = await axios.post(
-      "https://api.assemblyai.com/v2/transcript",
-      { audio_url: req.body.audio_url },
-      { headers: headers },
-    );
-    while (true) {
-      const getResponse = await axios.get(
-        `https://api.assemblyai.com/v2/transcript/${postResponse.data.id}`,
-        { headers },
-      );
-      if (getResponse.data.status === "completed") {
-        return res.json(getResponse.data);
-      } else if (getResponse.data.status === "error") {
-        throw new AssemblyAIError(400, getResponse.data.error); // Throw error from AAI server.
-      } else {
-        await new Promise((resolve) => setTimeout(resolve, 3000)); // Poll every 3 seconds.
-      }
+    const transcript = await client.transcripts.transcribe({
+      audio: req.body.audio_url
+    });
+    if (transcript.status === 'error') {
+      return res.status(500).json(transcript);
     }
-  } catch (error) {
-    res.status(error.response.status).json(error.response.data);
+
+    return res.json(transcript);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 });
 
 app.post("/upload", upload.single("file"), async (req, res) => {
   try {
-    const data = fs.readFileSync(req.file.path); // Read file from temp storage.
-    const response = await axios.post(
-      "https://api.assemblyai.com/v2/upload",
-      data,
-      { headers: headers },
-    );
-    fs.rmSync(req.file.path); // Remove file from temp storage.
-    res.json(response.data.upload_url);
-  } catch (error) {
-    res.status(error.response.status).json(error.response.data);
+    const uploadUrl = await client.files.upload(req.file.path);
+    await fs.rm(req.file.path); // Remove file from temp storage.
+    res.json(uploadUrl);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 });
 
